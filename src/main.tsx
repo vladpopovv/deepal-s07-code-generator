881216d19790ddfa0a770a1fe96679438f18ea1a
Vlad Popov <v4popov@gmail.com>
2025-10-06 17:30:25 +0500

    Add vercel react

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import App from './App';
 import './styles.css';
 
